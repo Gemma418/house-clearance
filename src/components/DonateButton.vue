7eb101d72f199c05640ae20d6d4eeb6ac62f3133
Gemma Lewington <g.a.lewington@gmail.com>
2025-08-25 18:35:03 +0100

    removed vue and back to html

--- src/components/DonateButton.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<div class="donate-button-container">
-            <a href="https://paypal.me/GemmaLewington" target="_blank" class="donate-button-link">Donate via PayPal</a>
-        </div>
-</template>
-
-<script setup lang="ts">
-// no props or logic needed yet
-</script>
